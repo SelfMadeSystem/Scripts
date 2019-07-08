0112598437f7129311f7b866c300a1acb3983b55
Sms_Gamer <creepercannyon@gmail.com>
2019-07-08 08:32:30 -0700

    Update CustomSettings.js

--- Customizable Stuff/CustomSettings.js
+++ Customizable Stuff/CustomSettings.js
@@ -5,7 +5,7 @@
 
 //At all times.
 var bhopSetSprinting = true; //Set sprinting?
-var bhopStrafe = false; //Want strafe?
+var bhopStrafe = true; //Want strafe?
 
 //When the player is on ground.
 var bhopMotionY1 = 0.42; //You can set this to basically anything. 0.42 is legit jump motion.
@@ -17,25 +17,32 @@ var bhopSpeedMultiply1 = 1.71 //Ground Speed. I think legit is 1.71
 //When the player isn't on ground.
 var bhopTimer2 = 1; //Timer. Default is 1
 var bhophClip2 = 0; //Do you want to hClip? Default 0.
-var bhopMotionYMultiply1 = 1.0; //You can set this to basically anything. Default1. The motionY multiply
+var bhopMotionY2 = 1; //You can set this to basically anything. Default1.
+var bhopMotionYType1 = 0; //Type of value that bhopMotionY1 will do to motionY. 0 is *= (multiply) 1 is += (add) 2 is -= (subtract) 3 is /= (divide) 4 is = (set)
 
 //When the player's motionY is lower than 0
-var bhopMotionYMultiply2 = 1.0; //You can set this to basically anything. Default1. The motionY multiply
+var bhopMotionY3 = 1; //You can set this to basically anything. Default1.
+var bhopMotionYType2 = 0; //Type of value that bhopMotionY2 will do to motionY. 0 is *= (multiply) 1 is += (add) 2 is -= (subtract) 3 is /= (divide) 4 is = (set)
 var bhopTimer3 = 1; //Timer. Default is 1
 var bhopAirSpeed1 = 0.02; //Sets your speed in air. 0.02 is default.
 
 //When the player's motionY is higher than 0.
-var bhopMotionYMultiply3 = 1.0; //You can set this to basically anything. Default1. The motionY multiply
+var bhopMotionY4 = 1; //You can set this to basically anything. Default1.
+var bhopMotionYType3 = 0; //Type of value that bhopMotionY3 will do to motionY. 0 is *= (multiply) 1 is += (add) 2 is -= (subtract) 3 is /= (divide) 4 is = (set)
 var bhopTimer4 = 1; //Timer. Default is 1
 var bhopAirSpeed2 = 0.02; //Sets your speed in air. 0.02 is default.
 
 //When it gets disabled.
 var bhopResetXZ = true; //When it gets disabled, do you want to reset MotionX and MotionZ to 0?
-var bhopResetY = false; //When it gets disabled, do you want to reset MotionY to 0?
+var bhopResetY = true; //When it gets disabled, do you want to reset MotionY to 0?
 
 
 //Custom Fly.
 
+
+//At all times.
+var flySetSprinting = true; //Set sprinting?
+var flyStrafe = false; //Want strafe?
 
 //When it gets enabled.
 var flyCheckGround1 = false; //Should it check for ground when doing these actions?
@@ -59,29 +66,29 @@ var flyMotionYType2 = 0; //Type of value that flyMotionY2 will do to motionY. 0 
 var flyMotionXZ2 = 1; //Value for motionX and Z.
 var flyMotionXZType2 = 0; //Type of value that flyMotionXZ2 will do to motionX and Z. 0 is *= (multiply) 1 is /= (divide)
 
-//If X ticks is % Z...
-var flyTicks1 = 4;
+//If X ticks is % Z... #1
+var flyTicks1 = 2;
 var flyTickse1 = 0;
 
-var flyMotionY3 = 1; //Value for motionY.
-var flyMotionYType3 = 0; //Type of value that flyMotionY3 will do to motionY. 0 is *= (multiply) 1 is += (add) 2 is -= (subtract) 3 is /= (divide) 4 is = (set)
+var flyMotionY3 = 0.3; //Value for motionY.
+var flyMotionYType3 = 4; //Type of value that flyMotionY3 will do to motionY. 0 is *= (multiply) 1 is += (add) 2 is -= (subtract) 3 is /= (divide) 4 is = (set)
 var flyMotionXZ3 = 1; //Value for motionX and Z.
 var flyMotionXZType3 = 0; //Type of value that flyMotionXZ3 will do to motionX and Z. 0 is *= (multiply) 1 is /= (divide)
 
-var flyTimer1 = 1;
+var flyTimer1 = 0.5;
 var flyVClip3 = 0;
 var flyHClip3 = 0;
 
 //If X ticks is % Z... #2
-var flyTicks2 = 4;
+var flyTicks2 = 2;
 var flyTickse2 = 1;
 
-var flyMotionY4 = 1; //Value for motionY.
-var flyMotionYType4 = 0; //Type of value that flyMotionY3 will do to motionY. 0 is *= (multiply) 1 is += (add) 2 is -= (subtract) 3 is /= (divide) 4 is = (set)
+var flyMotionY4 = -0.2; //Value for motionY.
+var flyMotionYType4 = 4; //Type of value that flyMotionY3 will do to motionY. 0 is *= (multiply) 1 is += (add) 2 is -= (subtract) 3 is /= (divide) 4 is = (set)
 var flyMotionXZ4 = 1; //Value for motionX and Z.
 var flyMotionXZType4 = 0; //Type of value that flyMotionXZ3 will do to motionX and Z. 0 is *= (multiply) 1 is /= (divide)
 
-var flyTimer2 = 1;
+var flyTimer2 = 0.5;
 var flyVClip4 = 0;
 var flyHClip4 = 0;
 
@@ -94,7 +101,7 @@ var flyMotionYType5 = 0; //Type of value that flyMotionY3 will do to motionY. 0 
 var flyMotionXZ5 = 1; //Value for motionX and Z.
 var flyMotionXZType5 = 0; //Type of value that flyMotionXZ3 will do to motionX and Z. 0 is *= (multiply) 1 is /= (divide)
 
-var flyTimer3 = 1;
+var flyTimer3 = 0.5;
 var flyVClip5 = 0;
 var flyHClip5 = 0;
 
@@ -107,6 +114,11 @@ var flyMotionYType6 = 0; //Type of value that flyMotionY3 will do to motionY. 0 
 var flyMotionXZ6 = 1; //Value for motionX and Z.
 var flyMotionXZType6 = 0; //Type of value that flyMotionXZ3 will do to motionX and Z. 0 is *= (multiply) 1 is /= (divide)
 
-var flyTimer4 = 1;
+var flyTimer4 = 0.5;
 var flyVClip6 = 0;
 var flyHClip6 = 0;
+
+
+//When it gets disabled.
+var flyResetXZ = false; //When it gets disabled, do you want to reset MotionX and MotionZ to 0?
+var flyResetY = false; //When it gets disabled, do you want to reset MotionY to 0?
